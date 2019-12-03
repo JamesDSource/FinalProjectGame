ccd7448a2517508b68bc6423b4293e48dcc8f796
lampperson <31216138+lampperson@users.noreply.github.com>
2019-12-03 13:19:03 -0800

    Initial commit

---
+++ app/src/main/java/com/example/finalprojectgame/Plane.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectgame;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Plane extends ImageView {
+    public Plane(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
